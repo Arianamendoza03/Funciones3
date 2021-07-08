8e6fee5738bb4338fa622697063f0183e486a8e1
ArianaMendonza03 <mendozaariana954@gmail.com>
2021-07-08 20:38:40 +1200

    funciones 3

---
+++ cmake-build-debug/CMakeFiles/funciones3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/funciones3.dir/main.cpp.obj"
+  "funciones3.exe"
+  "funciones3.exe.manifest"
+  "funciones3.pdb"
+  "libfunciones3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/funciones3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
